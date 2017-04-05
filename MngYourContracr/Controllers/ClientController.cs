4cd06f71e6b022b89aae93d88c359581ef2a3933
Stacy-D <stace1995d@gmail.com>
2017-04-05 12:09:33 -0700

    controllers added. waiting for refactoring

---
+++ MngYourContracr/Controllers/ClientController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace MngYourContracr.Controllers
+{
+    [Authorize(Roles = "Client")]
+    public class ClientController : Controller
+    {
+        //
+        // GET: /Client/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
