601ee4105c7484128f1d9c0170819be348d015a4
Stacy-D <stace1995d@gmail.com>
2017-04-11 14:28:08 -0700

    refactoring

--- MngYourContracr/Controllers/UsersController.cs
+++ MngYourContracr/Controllers/UsersController.cs
@@ -34,6 +34,18 @@ namespace MngYourContracr.Controllers
             }
             return false;
         }
+        public string getRole()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                CompanyContext context = new CompanyContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                    return s[0].ToString();
+            }
+            return null; 
+        }
         public ActionResult Index()
         {
             if (User.Identity.IsAuthenticated)
@@ -46,6 +58,7 @@ namespace MngYourContracr.Controllers
                 {
                     ViewBag.displayMenu = "Yes";
                 }
+                ViewBag.role = getRole();
                 return View();
             }
             else
